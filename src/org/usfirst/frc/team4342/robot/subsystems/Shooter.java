f5b152f28c45213dc5a9d731579ed3119a3884c8
Katie Schuetz <katie@theschuetzfamily.com>
2017-02-21 20:22:36 -0500

    shooter values

--- src/org/usfirst/frc/team4342/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team4342/robot/subsystems/Shooter.java
@@ -105,7 +105,7 @@ public class Shooter extends DemonSubsystem
 		isShooting = true;
 		
 		setFar();
-		setShooter(-1176);
+		setShooter(-850);
 	}
 	
 	/**
@@ -118,7 +118,7 @@ public class Shooter extends DemonSubsystem
 		isShooting = true;
 		
 		setClose();
-		setShooter(-750);
+		setShooter(-800);
 	}
 	
 	/**
